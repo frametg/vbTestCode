c774bb8e75c7c6cdac7957e8c9dcccfa105599b4
Settachat Tungpitagkai <59011345@kmitl.ac.th>
2021-02-10 15:31:53 +0700

    add login demo

---
+++ src/Module1.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Windows.Forms
+Module Module1
+
+    Sub Main()
+        Dim user As String = InputBox("input username", "login", "ppetech")
+        Dim passWord As String = InputBox("input passWord", "login", "ppetech")
+        MessageBox.Show("are you sure to login", "Login confirmation", MessageBoxButtons.OKCancel)
+        If (passWord = "ppetech") Then user = "success"
+        Console.WriteLine("ans: " + user)
+        Console.ReadLine()
+
+
+
+    End Sub
+
+End Module
---
+++ src/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("ConsoleApp1test")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("ConsoleApp1test")>
+<Assembly: AssemblyCopyright("Copyright ©  2021")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("d8710867-c50b-4b10-9785-6316c915d138")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
