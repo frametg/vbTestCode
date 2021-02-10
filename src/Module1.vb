ab88d1be039f534a9f32154e2451c4531f4e2011
Settachat Tungpitagkai <59011345@kmitl.ac.th>
2021-02-10 15:49:59 +0700

    add incorrect password

--- src/Module1.vb
+++ src/Module1.vb
@@ -4,9 +4,14 @@ Module Module1
     Sub Main()
         Dim user As String = InputBox("input username", "login", "ppetech")
         Dim passWord As String = InputBox("input passWord", "login", "ppetech")
+        Dim textDisplay As String
         MessageBox.Show("are you sure to login", "Login confirmation", MessageBoxButtons.OKCancel)
-        If (passWord = "ppetech") Then user = "success"
-        Console.WriteLine("ans: " + user)
+        If (passWord = "ppetech") Then
+            textDisplay = "loin as: " + user
+        Else
+            textDisplay = "incorrect password"
+        End If
+        Console.WriteLine(textDisplay)
         Console.ReadLine()
 
 
